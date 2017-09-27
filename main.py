e212c47740aa3f1e830e498bb9b233467464fba4
CaioGiacomelli <caio.giacomelli@hotmail.com>
2017-09-27 19:22:41 -0300

    Exclusão para um recurso, sem fila

--- main.py
+++ main.py
@@ -1,47 +1,51 @@
 import message
-from threading import Thread
 import threading
 import socket
 import pickle
 import copy
-import time
 import os
+import time
+
 
 class MyThread (threading.Thread):
 
-    def __init__(self, isack, pro, ts_ack):
+    def __init__(self, accept, pr, is_ack):
 
         threading.Thread.__init__(self)
-        self.isack = isack
-        self.pro = pro
-        self.ts_ack = ts_ack
+        self.accept = accept
+        self.pr = pr
+        self.is_ack = is_ack
         # self.name = name
         # self.counter = counter
 
     def run(self):
-        send_request(self.isack, self.pro, self.ts_ack)
+        send_request(self.accept, self.pr, self.is_ack)
 
 
-def send_request(isack, pro, ts_ack):
+def send_request(accept, pr, is_ack):
 
-    if not isack:
-        message_ts = str(pro.ts) + str(pro.pid)
-        message_ts = int(message_ts)
-        print("TS da mensagem enviada: ", message_ts)
-        for process in p:
-            tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            tcp.connect((host, process.port))
-            m1 = message.Message(message_ts, isack)
-            m_dumped = pickle.dumps(m1)
-            x = tcp.send(m_dumped)
-            tcp.close()
-        pro.ts += 1
+    if not accept:
+        for j in pr:
+            message_ts = str(p[j-1].ts) + str(p[j-1].pid)
+            message_ts = int(message_ts)
+            print("TS da mensagem enviada: ", message_ts)
+            for process in p:
+                tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                tcp.connect((host, process.port))
+                m1 = message.Message(message_ts, 0, accept)
+                m_dumped = pickle.dumps(m1)
+                x = tcp.send(m_dumped)
+                tcp.close()
+            p[j-1].ts += 1
 
     else:
-        for process in p:
+
+        print(pr)
+
+        for j in pr:
             tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            tcp.connect((host, process.port))
-            m1 = message.Message(ts_ack, isack)
+            tcp.connect((host, p[j-1].port))
+            m1 = message.Message(0, is_ack, 1) # Fazer depois a mensagem ACK ou NACK
             m_dumped = pickle.dumps(m1)
             x = tcp.send(m_dumped)
             tcp.close()
@@ -66,15 +70,20 @@ class Process:
         self.pid = pid
         self.ts = 1
         self.queue = []
-        self.acks = []
         self.process_list = []
+        self.ack = 0
+        self.nack = 0
         self.host = host
         self.port = port
+        self.recurso = 0
         thread2 = MyThread2(self)
         thread2.start()
 
     def set_process_list(self, process_list):
         self.process_list = process_list
+
+    def set_recurso(self):
+        self.recurso = 1
 
     def receive(self):
         tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -95,69 +104,92 @@ class Process:
                 if not msg: break
 
             con.close()
-            if not new_m.is_ack:
-                for ack in self.acks:
-                    if ack[0] - 1 == new_m.ts:
-                        new_m.count += 1
-                if new_m.count != len(self.process_list):
+            if not new_m.accept:
+                if self.recurso == 1:
+                    ts_process = str(self.ts) + str(self.pid)
+                    ts_process = int(ts_process)
 
-                    self.queue.append((new_m.ts, new_m))
-                    self.queue.sort(key=lambda tup: tup[0])
+                    if new_m.ts > ts_process:
+                        st = str(new_m.ts)
+                        y = st[(len(st) - 1):]
+                        x = [int(y)]
 
-                message_ts = str(self.ts) + str(self.pid)
-                message_ts = int(message_ts)
-                while new_m.ts > message_ts:
-                    self.ts += 1
-                    message_ts = str(self.ts) + str(self.pid)
-                    message_ts = int(message_ts)
-                thread3 = MyThread(1, self, (new_m.ts + 1))
-                thread3.start()
+                        print(self.pid, ts_process, new_m.ts, self.process_list[0].ts)
+
+                        if self.pid != x[0]:
+                            thread3 = MyThread(1, x, 0)
+                            thread3.start()
+
+                    if new_m.ts < ts_process:
+
+                        st = str(new_m.ts)
+                        y = st[(len(st) - 1):]
+                        x = [int(y)]
+
+                        print(self.pid, ts_process, new_m.ts, self.process_list[0].ts)
+                        if self.pid != x[0]:
+                            thread3 = MyThread(1, x, 1)
+                            thread3.start()
+
+                if self.recurso == 0:
+                    st = str(new_m.ts)
+                    y = st[(len(st) - 1):]
+                    x = [int(y)]
+
+                    print(self.pid)
+                    if self.pid != x[0]:
+                        thread3 = MyThread(1, x, 1)
+                        thread3.start()
+
             else:
-                found = False
 
-                for check in self.queue:
-                    if check[0] == new_m.ts - 1:
-                        found = True
-                        check[1].count += 1
-                        if check[1].count == len(self.process_list):
-                            print('Removendo da fila a mensagem com ts: ', new_m.ts-1, 'enviada para a porta: ', self.port)
-                            self.queue.remove(check)
+                if new_m.is_ack:
+                    self.ack+=1
+                else:
+                    self.nack+=1
 
-                if not found:
-                    print('ACK chegou antes')
-                    self.acks.append((new_m.ts, new_m))
+                ts_process = str(self.ts) + str(self.pid)
+                ts_process = int(ts_process)
 
+                if (self.ack + self.nack) == (len(self.process_list) - 1):
 
-host = '200.9.84.99'
-p = []
+                    if self.ack == (len(self.process_list) - 1) and self.recurso:
+                        print("Processo ", self.pid, " utilizou o recurso ", self.recurso, self.nack)
+                        self.ack = 0
+                        self.nack = 0
+                        self.recurso = 0
+
+                        for p in self.process_list:
+                            ts_p = str(p.ts) + str(p.pid)
+                            ts_p = int(ts_p)
+                            while(ts_process < ts_p):
+                                while ts_p > ts_process:
+                                    self.ts += 1
+                                    ts_process = str(self.ts) + str(self.pid)
+                                    ts_process = int(ts_process)
+                    else:
+                        self.ack = 0
+                        self.nack = 0
+
+host = '192.168.0.105'
 process_number = 1
+p = [Process(process_number, host, 5000), Process(process_number + 1, host, 5001), Process(process_number + 2, host, 5002)]
 
-print("1 - Cria Processo")
-print("2 - Enviar Mensagem")
-print("3 - Sair")
-option = input()
+for proc in p:
+    proc.set_process_list(p)
 
 while True:
-
-    if option == '1':
-        port_number = 5000 + process_number
-        p.append(Process(process_number, host, port_number))
-        process_number += 1
-        for proc in p:
-            proc.set_process_list(p)
-
-    elif option == '2':
-        print("Qual processo enviará a mensagem?")
-        sender = int(input())
-        thread1 = MyThread(0, p[sender-1], 0)
-        thread1.start()
-
-    elif option == '3':
-        os._exit(0)
-
-    print("1 - Cria Processo")
-    print("2 - Enviar Mensagem")
-    print("3 - Sair")
+    print("Digite os processos que desejam utilizar o recurso ou digite sair para fechar o programa")
     option = input()
 
+    if option == 'sair':
+        os._exit(0)
 
+    option = [int(i) for i in option.split()]
+    for o in option:
+        p[o-1].set_recurso()
+
+    thread1 = MyThread(0, option, 0)
+    thread1.start()
+
+    time.sleep(0.5)
--- message.py
+++ message.py
@@ -1,9 +1,10 @@
 from threading import Thread
-
+# TODO: fica a opção no option e ai fica mandando td pra quem n quer mais usar o recurso
 
 class Message:
 
-    def __init__(self, ts, is_ack):
+    def __init__(self, ts, is_ack, accept):
         self.ts = ts
         self.count = 0
         self.is_ack = is_ack
+        self.accept = accept
